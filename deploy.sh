a4f7ec025f2ecf584bbdfae52adc03aa3e5d2301
Adrian Philipp <mail@adrian-philipp.com>
2014-07-23 09:36:55 +0200

    Adds deploy script.

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# stick in .git/hooks/post-update and make executable
+
+echo "*** $USER: updating server info..."
+git update-server-info 2<&1
+
+echo "*** $USER: updating sources..."
+git reset --hard 2<&1
+
+echo "*** $USER: restarting nodejs..."
+/usr/bin/pkill -f '/usr/bin/node /home/whybug/whybug-server/server.js'
+/usr/bin/node /home/whybug/whybug-server/server.js
+
+echo "*** $USER: deployment done ***"
