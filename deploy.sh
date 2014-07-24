ce73e4c7ef38374bafe444ba6732cf0de0a8dc51
Adrian Philipp <mail@adrian-philipp.com>
2014-07-24 20:36:01 +0200

    Adds `npm install` to deploy script.

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
-# stick in .git/hooks/post-update and make executable
+# Call in .git/hooks/post-recieve and make executable
+# #!/bin/sh
+# cd ..
+# /bin/bash deploy.sh
+
+pwd
 
 echo "*** $USER: updating server info..."
 git update-server-info 2<&1
 
 echo "*** $USER: updating sources..."
 git reset --hard 2<&1
+
+echo "*** $USER: updating dependencies..."
+npm install
 
 echo "*** $USER: restarting nodejs..."
 # Todo: use cluster.js and send a signal to reload,
