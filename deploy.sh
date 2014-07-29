ed186191afaba45ec05ba1f00b5dbe606a2d9156
Adrian Philipp <mail@adrian-philipp.com>
2014-07-29 09:27:35 +0200

    Makes git commands work when called with git hooks.

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 # #!/bin/sh
 # cd .. && /bin/bash deploy.sh
 
-pwd
+unset GIT_DIR
 
 echo "*** $USER: updating server info..."
 git update-server-info 2<&1
@@ -14,9 +14,10 @@ git reset --hard 2<&1
 echo "*** $USER: updating dependencies..."
 npm install
 
-echo "*** $USER: restarting nodejs..."
+echo "*** $USER: restarting services..."
 # Todo: use cluster.js and send a signal to reload,
 /usr/bin/pkill -f '/usr/bin/node /home/whybug/whybug-server/server.js'
 /usr/bin/node /home/whybug/whybug-server/server.js 2<&1 > node-debug.log &
+varnishadm "ban req.url ~ /"
 
 echo "*** $USER: deployment done ***"
