a4e0e66772abe11f11172d8c044a57270facfc07
Adrian Philipp <mail@adrian-philipp.com>
2014-08-10 19:03:15 +0200

    Fix to make the uglifier use React.

--- src/web/static/js/app.js
+++ src/web/static/js/app.js
@@ -1,7 +1,7 @@
 import {WebApp} from '../../WebApp';
 
 // Declare React as global to make use of it in the Google Chrome extension.
-React = require('react');
+var React = require('react');
 React.renderComponent(WebApp({path: location.pathname}), document.getElementById('content-wrapper'));
 
 
