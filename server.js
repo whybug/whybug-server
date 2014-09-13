10425fc5f23432052b3458f38a0256dea0de7442
Adrian Philipp <mail@adrian-philipp.com>
2014-09-13 22:17:29 +0200

    Enables traceur experimental mode.

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
-//require('newrelic');
-
 var traceur = require('traceur');
 traceur.require.makeDefault(function(filename) {
   // don't transpile our dependencies, just our app
   return filename.indexOf('node_modules') === -1;
-});
+}, {experimental: true});
 
 require('./src/app.js');
