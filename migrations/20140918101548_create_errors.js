e82134160fb006257d46a529db758dbf634be798
Adrian Philipp <mail@adrian-philipp.com>
2014-09-20 15:55:33 +0200

    Adds checksum field to errors table.

--- migrations/20140918101548_create_errors.js
+++ migrations/20140918101548_create_errors.js
@@ -1,11 +1,9 @@
-'use strict';
-
-exports.up = function(knex, Promise) {
+exports.up = function(knex) {
   return knex.schema.createTable('errors', function (table) {
     table.uuid('uuid').primary();
-    table.uuid('errorgroup_uuid').index().notNullable();
     table.uuid('api_key').index();
     table.uuid('client_ip').notNullable();
+    table.string('checksum').notNullable();
     table.string('protocol_version').notNullable();
     table.string('level').notNullable();
     table.string('code').notNullable();
@@ -16,28 +14,10 @@ exports.up = function(knex, Promise) {
     table.string('os_version').notNullable();
     table.string('file_path').notNullable();
     table.integer('line').notNullable();
-    table.timestamp('created_at').notNullable().defaultTo('NOW');
-
+    table.timestamp('created_at').notNullable();
   });
 };
-/*
-.createTable('solutions', function(table) {
-    table.uuid('uuid').primary();
-    table.string('slug_long').notNullable().unique();
-    table.string('slug_short').notNullable().unique();
-    table.text('description');
-    table.string('context_level').notNullable();
-    table.string('context_code').notNullable();
-    table.string('context_message').notNullable();
-    table.string('context_programminglanguage').notNullable();
-    table.string('context_programminglanguage_version');
-    table.string('context_os').notNullable();
-    table.string('context_os_version').notNullable();
-    table.boolean('is_active').defaultTo(true);
-    table.timestamps();
-  })
-*/
 
-exports.down = function(knex, Promise) {
-  return knex.schema.dropTableIfExists('errorlogs')
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('errors')
 };
