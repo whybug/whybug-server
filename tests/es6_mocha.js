193d4ee0802c71dd6827abc29b59b58eb6104e06
Adrian Philipp <mail@adrian-philipp.com>
2014-10-19 18:59:27 +0200

    Adds chai react.

--- tests/es6_mocha.js
+++ tests/es6_mocha.js
@@ -2,10 +2,13 @@ var Traceur = require('traceur'),
     chai = require("chai"),
     sinonChai = require("sinon-chai"),
     chaiAsPromised = require("chai-as-promised"),
+    React = require('react/addons'),
+    chaiReact = require("chai-react"),
     should = chai.should();
 
 chai.use(sinonChai);
 chai.use(chaiAsPromised);
+chai.use(function (chai, utils) { return chaiReact(chai, utils, React) });
 
 // Traceur will compile all JS aside from node modules
 Traceur.require.makeDefault(function(filename) {
