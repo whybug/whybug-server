dfecfb72711a03d1f647221f6099dd9ceb83d3dc
Adrian Philipp <mail@adrian-philipp.com>
2014-10-19 15:40:43 +0200

    Absolute path to `/bin/bash`.

--- bin/metrics_travis.sh
+++ bin/metrics_travis.sh
@@ -9,7 +9,7 @@ if [ -e metrics_on_revision.csv ]; then
     rm metrics_on_revision.csv
 fi
 
-sh bin/run-command-on-git-revisions.sh master^ master "sh bin/metrics_on_this_rev.sh >> metrics_on_revision.csv"
+/bin/bash bin/run-command-on-git-revisions.sh master^ master "sh bin/metrics_on_this_rev.sh >> metrics_on_revision.csv"
 
 git checkout gh-pages
 cat metrics_on_revision.csv >> metrics.csv
