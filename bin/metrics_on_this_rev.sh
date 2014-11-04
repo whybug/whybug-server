4824a239df582db8220cfc85b8d656c1539d5fca
Adrian Philipp <mail@adrian-philipp.com>
2014-11-04 19:54:57 +0100

    Adds datetime to metrics.

--- bin/metrics_on_this_rev.sh
+++ bin/metrics_on_this_rev.sh
@@ -12,5 +12,8 @@ echo $rev,"js_unused",$unused_variables
 size=`du -k build/js/bundle.js | awk '{print $1}'`
 echo $rev,"js_size_kb",$size
 
+datetime=`date +"%Y-%m-%dT%H:%M:%S"`
+echo $rev,"datetime",$datetime
+
 # server startup time
 # build/css/main.css size
