aeb8a5f0555e06a813a087082dd8372f5be207bb
Adrian Philipp <mail@adrian-philipp.com>
2014-12-07 19:42:56 +0100

    Adds mock data for google provider profile.

---
+++ tests/domain/mocks-provider-profiles.js
@@ -0,0 +1,19 @@
+
+export var googleProfile = {
+  id: '103958218971419982951',
+  username: undefined,
+  displayName: 'Adrian Philipp',
+  name: { first: 'Adrian', last: 'Philipp' },
+  email: 'adrian.philipp@liip.ch',
+  raw:
+  { id: '103958218971419982951',
+    email: 'adrian.philipp@liip.ch',
+    verified_email: true,
+    name: 'Adrian Philipp',
+    given_name: 'Adrian',
+    family_name: 'Philipp',
+    link: 'https://plus.google.com/103958218971419982951',
+    picture: 'https://lh4.googleusercontent.com/-Deeha223uts/AAAAAAAAAAI/AAAAAAAAADA/Y7ulfkG_dFY/photo.jpg',
+    gender: 'male',
+    hd: 'liip.ch' }
+};
