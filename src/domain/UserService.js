b8ea654ab1a517b9447f55947d93bb26495baa3e
Adrian Philipp <mail@adrian-philipp.com>
2014-12-07 19:43:29 +0100

    Refactors login with provider to use new User and UserProfile.

--- src/domain/UserService.js
+++ src/domain/UserService.js
@@ -1,6 +1,7 @@
 import {UserProfile} from './UserProfile';
+import {User} from './User';
 
-var assert = require('chai').assert;
+var Joi = require('joi');
 
 export class UserService {
 
@@ -9,48 +10,41 @@ export class UserService {
     this.userProfileRepository = userProfileRepository;
   }
 
+  /**
+   * Tries to login a user with provided provider and profile.
+   *
+   * @param {string} provider Provider name, e.g. 'github', 'twitter'
+   * @param {Object} profile Profile returned from the service.
+   *
+   * @returns {Promise} Resolves with logged in User.
+   */
   async loginWithProvider(provider, profile) {
-    assert.include(UserProfile.providers(), provider, 'Invalid provider');
+    Joi.assert(provider, Joi.string().valid(UserProfile.providers()), 'Invalid provider');
 
-    // todo: validate provider
-    // todo: validate profile
-
-    // Move creation into repository?
-    //var userProfile = this.userProfileRepository.findOrCreateByProfile(provider, profile);
-    //
-    //// Update login counter.
-    //userProfile.save({login_count: userProfile.get('login_count') + 1}, {patch: true});
-    //
-    //var user = await userProfile.related('user');
-    //return user;
-
-    var user;
-    var userProfile = await this.userProfileRepository.findByProvider(provider, profile.id);
+    let user;
+    let userProfile = await this.userProfileRepository.findByProvider(provider, profile.id);
 
     if (!userProfile) {
-      user = (await user) || await this.userRepository.create({
-        email: profile.email,
-        display_name: profile.displayName,
-        avatar_url: profile.raw.avatar_url || profile.raw.profile_image_url_https || profile.raw.picture || ''
-      });
+      // If there is no UserProfile, this is a new user.
+      user = User.fromProvider(provider, profile);
+      userProfile = UserProfile.fromProvider(user.uuid, provider, profile);
 
-      userProfile = await this.userProfileRepository.create({
-        user_uuid: user.get('uuid'),
-        provider: provider,
-        external_id: profile.id,
-        username: profile.username || profile.displayName,
-        display_name: profile.displayName,
-        email: profile.email,
-        avatar_url: profile.raw.avatar_url || profile.raw.profile_image_url_https || profile.raw.picture || '',
-        profile_url: profile.raw.html_url || profile.raw.url || '',
-        last_login_at: new Date(),
-        login_count: 1
-      });
+      await Promise.all([
+        this.userRepository.store(user),
+        this.userProfileRepository.store(userProfile)
+      ]);
+
+    } else {
+      // Otherwise we have an existing user.
+      userProfile.updateLastLogin();
+
+      [user] = await Promise.all([
+        this.userRepository.findByUuid(userProfile.user_uuid),
+        this.userProfileRepository.store(userProfile, true)
+      ]);
     }
 
-    userProfile.save({login_count: userProfile.get('login_count') + 1}, {patch: true});
-
-    return user || userProfile.related('user');
+    return user;
   }
 }
 
--- tests/domain/UserServiceTest.js
+++ tests/domain/UserServiceTest.js
@@ -1,37 +1,56 @@
 import {UserService} from '../../src/domain/UserService';
+import {User} from '../../src/domain/User';
+import {UserProfile} from '../../src/domain/UserProfile';
+
+import {googleProfile} from './mocks-provider-profiles';
 
 var sinon = require("sinon");
 
 describe('UserService', () => {
-  var userRepository = sinon.stub();
-  var userProfileRepository = sinon.stub();
-  var userService = new UserService(userRepository, userProfileRepository);
-
-  var githubUser = {
-
+  var userRepository = {
+    findByUuid: sinon.stub(),
+    store: sinon.stub()
   };
+  var userProfileRepository = {
+    findByProvider: sinon.stub(),
+    store: sinon.stub()
+  };
+  var userService = new UserService(userRepository, userProfileRepository);
 
   describe('#loginWithProvider()', () => {
 
-    it('should validate provider', async () => {
-      try {
-        var user = await userService.loginWithProvider('wrong-provider', {});
-      } catch(exception) {
-        exception.message.should.have.string("Invalid provider");
-      }
+    it('should validate the provider', () => {
+      return userService
+        .loginWithProvider('wrong-provider', {})
+        .should.eventually.be.rejectedWith('Invalid provider');
     });
 
-    it('should login with github', async () => {
-      //var user = await userService.loginWithProvider('github', {});
-      //userRepository.findByProvider('');
-    })
+    it('should login with new google profile', () => {
+      userProfileRepository.findByProvider.returns(null);
+
+      return userService.loginWithProvider('github', googleProfile).should.be.fulfilled
+        .then(() => {
+          userRepository.store.should.have.been.calledWith(sinon.match.instanceOf(User));
+          userProfileRepository.store.should.have.been.calledWith(sinon.match.instanceOf(UserProfile));
+        });
+    });
+
+    it('should login with signed up google profile', () => {
+      var userProfile = {
+        updateLastLogin: sinon.stub()
+      };
+      userProfileRepository.findByProvider.returns(userProfile);
+
+      return userService.loginWithProvider('github', googleProfile).should.be.fulfilled
+        .then(() => {
+          userProfile.updateLastLogin.should.have.been.calledWith(sinon.match.any);
+          userProfileRepository.store.should.have.been.calledWith(userProfile, true);
+        });
+    });
 
     /*
-     should validate profile
      should login with twitter
-     should login with google
+     should login with github
      */
-
-
   });
 });
