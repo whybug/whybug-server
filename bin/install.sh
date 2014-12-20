b2cf4696f7d275ec8d0cf6447ef64c4aa42ba747
Adrian Philipp <mail@adrian-philipp.com>
2014-12-20 15:54:18 +0100

    Allows installing of pm2 as root.

--- bin/install.sh
+++ bin/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "updating dependencies..."
-npm install
+npm install --unsafe-perm
 
 echo "building assets..."
 node_modules/.bin/webpack --config config/webpack.config.js -p 2<&1
