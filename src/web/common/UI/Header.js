c55200eae5b2dd4f7e0598ced4c47ed8d147cfb5
Adrian Philipp <mail@adrian-philipp.com>
2015-05-16 21:55:07 +0200

    Commit missing files (case sensitive).

---
+++ src/web/common/UI/Header.js
@@ -0,0 +1,64 @@
+/**
+ * @flow
+ */
+var React = require('react'),
+    config = require('../../../../config/config'),
+    routes = require('../../../../config/routes');
+
+import {
+  Container,
+  Column,
+  Link,
+  Navigation,
+  NavLink,
+} from '../UI';
+
+export class Header {
+  render()  {
+    return (
+      <header className="navbar-section">
+        <Container>
+          <div className="w-nav w-row navbar">
+            <Column span={1}>
+              <Link to="startpage" className="w-nav-brand brand">whybug</Link>
+            </Column>
+
+            <Column span={11}>
+              <Navigation>
+                <NavLink to="search">Solutions</NavLink>
+                <NavLink to="language_index">Installation</NavLink>
+                {this.props.user ? this.renderUserMenu() : this.renderLoginMenu()}
+              </Navigation>
+            </Column>
+          </div>
+        </Container>
+      </header>
+    );
+  }
+
+  renderUserMenu() {
+    return (
+      <div className="w-nav-link nav-link">
+        <img className="avatar" src={this.props.user.avatar_url} width="25" height="25" />
+        <div className="subnav">
+          <a href={routes.web.logout.path}><span>Account</span></a>
+          <a href={routes.web.logout.path}><span>Logout</span></a>
+        </div>
+      </div>
+    );
+  }
+
+  renderLoginMenu() {
+    return (
+      <div className="w-nav-link nav-link">
+        <span>Login</span>
+
+        <div className="subnav">
+          <a href={routes.web.login.github.path} className="icon-github-round"><span>Github</span></a>
+          <a href={routes.web.login.twitter.path} className="icon-twitter-square"><span>Twitter</span></a>
+          <a href={routes.web.login.google.path} className="icon-google-square"><span>Google</span></a>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/web/common/UI/Markdown.js
@@ -0,0 +1,17 @@
+var React = require('react'),
+    marked = require('marked');
+
+var {div} = React.DOM;
+
+/**
+ * Renders markdown.
+ */
+export var Markdown = React.createClass({
+  render() {
+    return (
+      <div
+        className='markdown-body'
+        dangerouslySetInnerHTML={{'__html': marked(this.props.text)}} />
+    );
+  }
+});
