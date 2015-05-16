4032bbd4e51437f6f4f3349ba670024d9d836ad8
Adrian Philipp <mail@adrian-philipp.com>
2015-05-16 17:48:12 +0200

    Pack sass file and optimize for production.

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,27 +1,23 @@
 var webpack = require('webpack');
+var path = require('path');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-  entry: {
-    bundle: './src/web/assets/js/app.js'
-  },
+  entry: [
+    './src/web/assets/js/app.js',
+    './src/web/assets/css/main.sass'
+  ],
   output: {
-    path: 'build/',
+    path: path.join(__dirname, '/../build/'),
     publicPath: process.env.WEB_URL,
     filename: '[name].js'
   },
   module: {
     loaders: [
-      // Hand in environment variables.
-      {
-        test: /config\/.*\.js$/,
-        loader: 'envify'
-      },
-
       // Compile javascript ES6/ES7/JSX code.
       {
         test: /(src|config)\/.*\.js$/,
-        loader: 'babel-loader?experimental=true&playground=true&optional=runtime'
+        loader: 'babel-loader?stage=0&optional=runtime'
       },
 
       // Compile assets.
@@ -38,7 +34,10 @@ module.exports = {
     // Set a global variable to indicate if the javascript
     // code is executed in the browser.
     new webpack.DefinePlugin({
-      __BROWSER__: true
+      __BROWSER__: true,
+      "process.env": {
+        "NODE_ENV": JSON.stringify("production") // This has effect on the react lib size
+      }
     }),
 
     new ExtractTextPlugin("[name].css")
