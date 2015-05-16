d009ece0c840cd8488e9e72f34bc42ef0bff0b30
Adrian Philipp <mail@adrian-philipp.com>
2015-05-16 17:50:58 +0200

    Use react-router in WebApp.

--- src/web/WebApp.js
+++ src/web/WebApp.js
@@ -1,54 +1,12 @@
 var React = require('react'),
-    Async = require('react-async'),
-    Router = require('react-router-component'),
+    Router = require('react-router'),
     config = require('../../config/config'),
     routes = require('../../config/routes');
 
-var {Location, Locations} = Router;
+var {RouteHandler} = Router;
 
-import {StartPage} from './start/StartPage';
-import {SolutionCreatePage} from './solutions/SolutionCreatePage';
-import {SolutionViewPage} from './solutions/SolutionViewPage';
-import {SolutionSearchPage} from './solutions/SolutionSearchPage';
-import {InstallationPage} from './installation/InstallationPage';
-import {NotFoundPage} from './common/NotFoundPage';
-
-
-export var WebApp = React.createClass({displayName: 'WebApp',
-
-  mixins: [ Async.Mixin ],
-
-  getInitialStateAsync(callback) {
-    callback(null, {
-      user: this.props.user,
-      params: this.props.params,
-      query: this.props.query
-    });
-  },
-
-  getPages() {
-    return [
-      {path: routes.web.startpage.path, handler: StartPage},
-      {path: routes.web.solution.search.path, handler: SolutionSearchPage},
-      {path: routes.web.solution.create.path, handler: SolutionCreatePage},
-      {path: routes.web.solution.view.path, handler: SolutionViewPage},
-      {path: routes.web.installation.index.path, handler: InstallationPage},
-      {path: routes.web.installation.language.path, handler: InstallationPage},
-      {path: null, handler: NotFoundPage}
-    ];
-  },
-
+export var WebApp = React.createClass({
   render() {
-    return this.transferPropsTo(
-      Locations({path: this.props.path},
-        this.getPages().map((page) => Location({
-          path: page.path,
-          handler: page.handler,
-          user: this.state.user,
-          params: this.state.params,
-          query: this.state.query
-        }))
-      )
-    );
+    return <RouteHandler {...this.props} />;
   }
 });
