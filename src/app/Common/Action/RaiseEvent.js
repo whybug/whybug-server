9d330377f6a2732213ac374a1fa558444c5d3e2d
Adrian Philipp <mail@adrian-philipp.com>
2015-08-30 15:19:08 +0200

    Experiment with flow types for action.

--- src/app/Common/Action/RaiseEvent.js
+++ src/app/Common/Action/RaiseEvent.js
@@ -1,6 +1,4 @@
 /* @flow weak */
-var Joi = require('joi');
-
 import {RAISE_EVENT} from '../../Constants';
 import {INTERNAL_SOURCES, INTERNAL} from '../Domain/Sources';
 
@@ -8,7 +6,7 @@ export function raiseEvent(event, action) {
   return {type: RAISE_EVENT, source: INTERNAL, action, event };
 }
 
-export function raiseEventValidator() {
+export function raiseEventValidator(Joi) {
   return {
     type: Joi.string().valid(RAISE_EVENT),
     source: Joi.string().valid(INTERNAL_SOURCES),
--- src/app/User/Action/SendWelcomeEmail.js
+++ src/app/User/Action/SendWelcomeEmail.js
@@ -1,22 +1,30 @@
-/* @flow weak */
+/* @flow */
 import {SEND_WELCOME_EMAIL} from '../../Constants';
 import {INTERNAL_SOURCES, INTERNAL} from '../../Common/Domain/Sources';
 
-type SendWelcomeEmail = {
-  type: 'SEND_WELCOME_EMAIL';
-  userId: number;
+type SendWelcomeEmailAction = {
+  type: string;
+  source: string;
+  userId: string;
   email: string;
 };
 
-export function sendWelcomeEmail(userId, email): SendWelcomeEmail {
-  return { type: SEND_WELCOME_EMAIL, source: INTERNAL, userId, email };
+export function sendWelcomeEmail(userId: string, email: string): SendWelcomeEmailAction {
+  return {
+    type: SEND_WELCOME_EMAIL,
+    source: INTERNAL,
+    userId,
+    email
+  };
 }
 
-export function sendWelcomeEmailValidator(Joi) {
+export function sendWelcomeEmailValidator(Joi: any): SendWelcomeEmailAction {
   return {
-    type: Joi.string().valid(SEND_WELCOME_EMAIL).required(),
-    source: Joi.string().valid(INTERNAL_SOURCES).required(),
+    type: Joi.string().valid(SEND_WELCOME_EMAIL),
+    source: Joi.string().valid(INTERNAL_SOURCES),
     userId: Joi.string().guid(),
+    //firstName: Joi.string().max(255),
+    //lastName: Joi.string().max(255),
     email: Joi.string().email().max(255)
   };
 }
