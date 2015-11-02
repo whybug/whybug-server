ebb607d74b5e09b4933ef3f817e9ea2006aadaad
Adrian Philipp <mail@adrian-philipp.com>
2015-11-02 22:02:41 +0100

    Refactors dependencies

---
+++ tests/dependencies.js
@@ -0,0 +1,11 @@
+var chai = require("chai"),
+  sinonChai = require("sinon-chai"),
+  chaiAsPromised = require("chai-as-promised"),
+  React = require('react/addons'),
+  chaiReact = require("chai-react"),
+  should = chai.should();
+
+chai.use(sinonChai);
+chai.use(chaiAsPromised);
+chai.use(function (chai, utils) { return chaiReact(chai, utils, React) });
+
--- tests/es6_mocha.js
+++ tests/es6_mocha.js
@@ -1,13 +1,3 @@
-var chai = require("chai"),
-    sinonChai = require("sinon-chai"),
-    chaiAsPromised = require("chai-as-promised"),
-    React = require('react/addons'),
-    chaiReact = require("chai-react"),
-    should = chai.should();
-
-chai.use(sinonChai);
-chai.use(chaiAsPromised);
-chai.use(function (chai, utils) { return chaiReact(chai, utils, React) });
-
 require("babel/register")();
+require('./dependencies');
 require('../src/server/index.js');
