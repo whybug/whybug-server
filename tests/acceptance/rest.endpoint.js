45173b36703c594a4a7a72ab72c481549f59e44e
Adrian Philipp <mail@adrian-philipp.com>
2015-11-02 22:02:08 +0100

    Adds smoke test

--- tests/acceptance/rest.endpoint.js
+++ tests/acceptance/rest.endpoint.js
@@ -1,17 +1,30 @@
-var request = require('supertest');
+var request = require('supertest-as-promised');
 
 import {signUpUser} from '../../src/app/User/Action/SignUpUser';
 
 describe('Rest endpoint', () => {
 
+  describe('startpage', () => {
+    it('smoke test', () =>
+      request('localhost:8000')
+        .get('/')
+        .set('Accept', 'text/html')
+        .expect(200)
+        .expect((res) => {
+          res.body.should.be.string
+          //res.body.should.contain('navbar-section')
+        })
+    )
+  });
+
   describe('POST /api/rest/actions', () => {
-    it('takes signup action', (done) => {
+    it('takes signup action', () =>
       request('localhost:8000')
         .post('/api/rest/actions')
         .set('Accept', 'application/json')
         .send(signUpUser({loginService: "google"}))
-        .expect(200, done);
-    })
+        .expect(200, {})
+    )
   });
 
 
