419b614be2413028d2c2517e0cdd073a815a9fab
Adrian Philipp <mail@adrian-philipp.com>
2015-11-06 10:34:08 +0100

    Moves rest helper methods in its own file

--- src/server/rest.js
+++ src/server/rest.js
@@ -1,5 +1,6 @@
 /* @flow weak */
 
+import {wrapDispatch, wrapQuery, wrapRoute} from './rest.utils';
 import {searchSolutions} from '../app/Solution/Query/SearchSolutions';
 import {recordError} from '../app/Error/Action/RecordError';
 import {findSolutionsForError} from '../app/Solution/Query/FindSolutionForError';
@@ -33,55 +34,3 @@ module.exports = (express, store, routes) => {
 
   return app;
 };
-
-function wrapQuery(store, query) {
-  return store.query(setSource(query));
-}
-
-function wrapDispatch(store, action) {
-  return store.dispatch(setSource(action));
-}
-
-/**
- * Catch errors and transform them to responses.
- *
- * @param {Object} app Express app
- * @param {{method: string, path: string}} route
- * @param {Func} handler Callback returning promise.
- */
-function wrapRoute(app, route, handler: Function) {
-  var method = route.method.toLowerCase();
-
-  app[method](route.path, async (req, res) => {
-    try {
-      res.status(200).send(await handler(req, res));
-    } catch(e) {
-      res.status(400).send(formatException(e, req.body));
-    }
-  });
-}
-
-/**
- * Sets source of the action/query/event to rest.
- *
- * @param item
- * @returns {{source: string}}
- */
-function setSource(item) {
-  return {...item, source: 'rest'};
-}
-
-/**
- * Formats an exception to a JSON response.
- *
- * @param {Error} e exception object
- * @param {Object} payload Payload sent to the API.
- * @returns {{error: string, details: Object, payload: Object}}
- */
-function formatException(e, payload = {}) {
-  return {
-    error: e.message || 'Unknown error',
-    details: e.details || {},
-    payload: payload
-  };
-}
---
+++ src/server/rest.utils.js
@@ -0,0 +1,51 @@
+export function wrapQuery(store, query) {
+  return store.query(setSource(query));
+}
+
+export function wrapDispatch(store, action) {
+  return store.dispatch(setSource(action));
+}
+
+/**
+ * Catch errors and transform them to responses.
+ *
+ * @param {Object} app Express app
+ * @param {{method: string, path: string}} route
+ * @param {Func} handler Callback returning promise.
+ */
+export function wrapRoute(app, route, handler: Function) {
+  var method = route.method.toLowerCase();
+
+  app[method](route.path, async (req, res) => {
+    try {
+      res.status(200).send(await handler(req, res));
+    } catch(e) {
+      res.status(400).send(formatException(e, req.body));
+    }
+  });
+}
+
+/**
+ * Sets source of the action/query/event to rest.
+ *
+ * @param item
+ * @returns {{source: string}}
+ */
+function setSource(item) {
+  return {...item, source: 'rest'};
+}
+
+/**
+ * Formats an exception to a JSON response.
+ *
+ * @param {Error} e exception object
+ * @param {Object} payload Payload sent to the API.
+ * @returns {{error: string, details: Object, payload: Object}}
+ */
+function formatException(e, payload = {}) {
+  return {
+    error: e.message || 'Unknown error',
+    details: e.details || {},
+    payload: payload
+  };
+}
