a1b5d0d159e078df74c4cf67d1e073abd2252b86
Adrian Philipp <mail@adrian-philipp.com>
2015-12-21 11:32:14 +0100

    Adds draft for react-router switch

--- src/server/react.js
+++ src/server/react.js
@@ -20,6 +20,26 @@ module.exports = (express) => {
 
     app.use(express.static(__dirname + '/../../build/'));
 
+    // Todo: switch to react-router >=1.0
+    //import { renderToString } from 'react-dom/server'
+    //import { match, RouterContext } from 'react-router'
+    //import routes from './routes'
+    //
+    //serve((req, res) => {
+    //    // Note that req.url here should be the full URL path from
+    //    // the original request, including the query string.
+    //    match({ routes, location: req.url }, (error, redirectLocation, renderProps) => {
+    //        if (error) {
+    //            res.status(500).send(error.message)
+    //        } else if (redirectLocation) {
+    //            res.redirect(302, redirectLocation.pathname + redirectLocation.search)
+    //        } else if (renderProps) {
+    //            res.status(200).send(renderToString(<RouterContext {...renderProps} />))
+    //        } else {
+    //            res.status(404).send('Not found')
+    //        }
+    //    })
+    //})
     app.get('*', (req, res) => {
         let webRoutes = new WebRoutes(req.path);
         webRoutes.getMarkup((markup, data = {}) => {
