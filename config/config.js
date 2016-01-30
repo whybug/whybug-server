44bc7b88afff8067ffd7cc1d48f6e77136c559c4
Adrian Philipp <mail@adrian-philipp.com>
2016-01-30 12:47:12 +0100

    Adds es host to env vars

--- config/config.js
+++ config/config.js
@@ -36,7 +36,7 @@ if (typeof __BROWSER__ == 'undefined' || __BROWSER__ === false) {
   };
 
   config.elasticsearch = {
-    host: process.env.ES_URL || '127.0.0.1',
+    host: process.env.ES_HOST || '127.0.0.1',
     port: process.env.ES_PORT || 9200
   };
 
