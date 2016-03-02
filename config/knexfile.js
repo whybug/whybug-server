164983d92bc4ba9141f1d00b85d680c41d5f6e57
Adrian Philipp <mail@adrian-philipp.com>
2016-03-02 16:10:44 +0100

    Typo

--- config/knexfile.js
+++ config/knexfile.js
@@ -2,13 +2,13 @@ var config = require('./config');
 
 module.exports = {
   production: {
-    client: 'mysql',
+    client: 'mysql2',
     connection: {
       host:     config.mysql.host,
       port:     config.mysql.port,
       database: config.mysql.db,
       user:     config.mysql.user,
-      password: config.mysql.pass
+      password: config.mysql.password,
     },
     pool: {
       min: 2,
